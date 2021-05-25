9e5c79ad83a7e669ce0a9760d372531e3472cf2d
jaditya8109 <jaditya8109@gmail.com>
2021-05-25 19:38:04 +0530

    basic ui setup and form add and delete working

---
+++ client/src/components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Contacts from './Contacts';
+import ContactForm from './ContactForm';
+
+const Home = () => {
+    return (
+        <div className="grid-2">
+            <div>
+                <ContactForm />
+            </div>
+            <div>
+                <Contacts />
+            </div>
+        </div>
+    )
+}
+
+export default Home;
