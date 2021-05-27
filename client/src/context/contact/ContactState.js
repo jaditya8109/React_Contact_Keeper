95d7cfdb3c4ecf4d9af613a6114c967709866ef5
jaditya8109 <jaditya8109@gmail.com>
2021-05-28 00:13:47 +0530

    UI implemented but not connected with backend

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -31,7 +31,9 @@ import {ADD_CONTACT, DELETE_CONTACT,
                   phone: '333-333-333',
                   type: 'professional'
                 }
-              ]
+              ],
+              current: null,
+              filtered: null
             };
           
             const [state, dispatch] = useReducer(contactReducer, initialState);
@@ -47,24 +49,44 @@ import {ADD_CONTACT, DELETE_CONTACT,
               dispatch({type: DELETE_CONTACT, payload:id});
             }
           
-          
             // Set Current Contact
+            const setCurrent = contact =>{
+              dispatch({type: SET_CURRENT, payload: contact});
+            }
           
             // Clear Current Contact
+            const clearCurrent = () =>{
+              dispatch({type: CLEAR_CURRENT});
+            }
           
             // Update Contact
+            const updateContact = contact => {
+              dispatch({ type: UPDATE_CONTACT, payload: contact });
+            };
           
             // Filter Contacts
+            const filterContacts = text => {
+              dispatch({ type: FILTER_CONTACTS, payload: text });
+            };
           
             // Clear Filter
+            const clearFilter = () => {
+              dispatch({ type: CLEAR_FILTER });
+            };
           
             return (
               <ContactContext.Provider
                 value={{
                   contacts: state.contacts,
+                  current: state.current,
+                  filtered: state.filtered,
                   addContact,
                   deleteContact,
-                  
+                  setCurrent,
+                  clearCurrent,
+                  updateContact,
+                  clearFilter,
+                  filterContacts
                 }}
               >
                 {props.children}
